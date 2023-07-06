9d10ee669e3d19b5b7d120608066e54325dda699
Rex Holmes <holmesrex515@gmail.com>
2023-07-07 04:01:48 +0900

    part 4 complete

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import MainDisplay from "./Components/MainDisplay";
 import OptionDisplay from "./Components/OptionDisplay";
 import { selectPotentials } from "./redux/slices/potentialCountriesSlice";
 import { useSelector } from "react-redux";
+import { selectDisplay } from "./redux/slices/displayCountrySlice";
 
 function App() {
     let potentials = useSelector(selectPotentials)
+    let currentDisplay = useSelector(selectDisplay)
     console.log(potentials)
+    console.log("DISPLAY", currentDisplay)
+
     return (
         <div className="App font-link">
             <Header />
