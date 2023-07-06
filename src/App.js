e5d58bc5347e4e65b0f7b0e986eec95cfcf746a0
Rex Holmes <holmesrex515@gmail.com>
2023-07-07 04:16:20 +0900

    part 5 complete

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
     return (
         <div className="App font-link">
             <Header />
-            <OptionDisplay />
+            {currentDisplay ? <MainDisplay /> : <OptionDisplay />}
         </div>
     );
 }
